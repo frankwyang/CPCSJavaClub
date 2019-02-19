571a0eb68c321db29cac3868b3c8e754b8ec73cd
嘉佳 <frankwyang@users.noreply.github.com>
2019-02-19 09:01:09 -0800

    practicing Git
    added method1() and method2()

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,21 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        int x = 4;
+        int x = 3;
         if(x == 4)
         {
             System.out.println("Hello World!");
+            method1();
         }        
+        method2();
+    }
+
+    public static void method1()
+    {
+        System.out.println("Blah!");
+    }
+    
+    private static void method2()
+    {
+        System.out.println("Blah Blah!!");
     }
 }
