a667ca4903e602eb53387bd47f1a9c7b64203abd
嘉佳 <frankwyang@users.noreply.github.com>
2019-02-19 08:42:10 -0800

    added { } to if statement for style purpose

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        int x = 4;
+        if(x == 4)
+        {
+            System.out.println("Hello World!");
+        }        
+    }
+}
